5342bf9e1886d41cb5782884bc801272131104d1
danferr99 <69489616+danferr99@users.noreply.github.com>
2021-06-28 18:56:03 -0300

    asd

--- src/infra/database.js
+++ src/infra/database.js
@@ -2,9 +2,9 @@ require('dotenv').config({ path: './src/config/.env' });
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    console.log(process.env.DB_CONNECTION);
+    //console.log(process.env.DB_CONNECTION);
     try {
-        await mongoose.connect(process.env.DB_CONNECTION, {
+        await mongoose.connect("mongodb+srv://DanFerr:daniel88234656@cluster0.nyw9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
